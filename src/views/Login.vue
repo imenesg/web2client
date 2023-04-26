110de74dbf69f9446cc878ffd48453978868b239
Gabriel Imenes <69609443+imenesg@users.noreply.github.com>
2023-04-25 21:56:35 -0300

    Add files via upload

---
+++ src/views/Login.vue
@@ -0,0 +1,21 @@
+<script>
+
+</script>
+
+<template>
+    <section class="d-flex align-self-center justify-content-center w-100">
+        <div class="card text-center mt-5" style="width: 30.5rem;">
+            <div class="card-body">
+                <form class="d-flex align-self-center justify-content-center flex-column">
+                    <h5 class="card-title">Login</h5>
+                    <input class="form-control mt-3" type="text" placeholder="Usuario" aria-label="">
+                    <input class="form-control mt-3" type="text" placeholder="Senha" aria-label="">
+                    <button class="btn btn-primary mt-3 mb-4">Logar</button>
+                    <a href="#" class="">Criar conta</a>
+                </form>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style></style>
